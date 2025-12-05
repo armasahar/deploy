7f579bb62041edd12440d665909c4fc2641c6f7c
armasahar <armasahar.code@gmail.com>
2025-12-05 20:19:53 +0530

    final code

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-import express from 'express'
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import {fileURLToPath} from 'url';
 
-const server = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const port = 3002
+const PORT = 5600;  
+const app = express();
 
-server.get('/', (req, res)=>{
-    res.send("Hello from VM")
-    res.status(200)
+const buildPath = path.join(__dirname, "build");
+if(fs.existsSync(buildPath)){
+  app.use(express.static(buildPath));
+  app.get(/^\/(?!api).*/, (req,res)=>{
+    res.sendFile(path.join(buildPath, "index.html"));
+  })
+}
+
+app.use(express.json());
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Express.js server!' });
 })
 
-server.listen(port, ()=>{
-    console.log('Hello from VM server');
-})
+// app.use('./', (req, res) => {
+//   res.json(message'')
+// } )
+app.use((req, res) => {
+  res.status(404).send("404 - Not Found");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("500 - Internal Server Error");
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
